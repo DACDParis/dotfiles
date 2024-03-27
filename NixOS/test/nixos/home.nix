267739d2afb42705a945a3c361a1e7db4a8f5b16
dacdparis <dacdparis@gmail.com>
2024-03-27 18:30:17 +0100

    test folder for nix configs

---
+++ NixOS/test/nixos/home.nix
@@ -0,0 +1,84 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  home.username = "david";
+  home.homeDirectory = "/home/david";
+
+  # link the configuration file in current directory to the specified location in home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  # set cursor size and dpi for 4k monitor
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    "Xft.dpi" = 172;
+  };
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    neofetch
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+    nix-output-monitor
+    btop  # replacement of htop/nmon
+    iotop # io monitoring
+    iftop # network monitoring
+    ltrace # library call monitoring
+    lsof # list open files
+    sysstat
+    lm_sensors # for `sensors` command
+    ethtool
+    pciutils # lspci
+    usbutils # lsusb
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "dacdparis";
+    userEmail = "dacdparis@gmail.com";
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    # TODO add your custom bashrc here
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '';
+
+    shellAliases = {
+    };
+  };
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
